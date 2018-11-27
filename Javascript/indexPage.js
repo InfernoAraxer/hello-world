adeb2f3a9cbb0bba491e78428c28eb06401a05e5
Alex Tan <AT212355@gmail.com>
2018-11-27 18:06:17 -0500

    Added 4 Buttons

---
+++ Javascript/indexPage.js
@@ -0,0 +1,14 @@
+function one(){
+  console.log("Hello, World!");
+}
+function two(){
+  alert("Hello, World!");
+}
+function three(){
+  var p = document.getElementById("Hello");
+  p.innerHTML = "Hello, World!";
+}
+function four(){
+  var name = prompt("Please enter your name");
+  alert('Hello, ' + name);
+}
